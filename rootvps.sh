a43480339ffba493afdb24217d2a634352a61287
prabowo-tunnel <prantaspramip@gmail.com>
2025-03-30 19:50:29 +0700

    Update rootvps.sh

--- rootvps.sh
+++ rootvps.sh
@@ -1,31 +1,12 @@
-# ipinfo
-MYIP=$(wget -qO- ipinfo.io/ip);
-# pewarna hidup
-Bred="\e[1;31m"
-BGreen='\e[1;32m'
-BYellow='\e[1;33m'
-BBlue='\e[1;34m'
-BPurple='\e[1;35m'
-NC='\e[0m'
-wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/prabowo-tunnel/rootvps/main/sshd_config;
-systemctl restart sshd;
-clear;
-read -p "Enter Password : " passwd
-clear
-if [[ ! -z "${passwd}" ]]; then
-echo ""
-echo -e "\e[1;34m------------------------------------------\e[0m"
-echo -e "\e[1;31m Please Save This VPS Account Information\e[0m"
-echo -e "\e[1;34m------------------------------------------\e[0m"
-echo -e "\e[1;32mIp address \e[0m = $MYIP"
-echo -e "\e[1;32mPort SSH   \e[0m = 22"
-echo -e "\e[1;32mUsername   \e[0m = root"
-echo -e "\e[1;34m-------------------------------------------"
-echo -e "\e[1;32m    Successfully enable root...!!!\e[0m"
-echo -e "\e[1;34m-------------------------------------------"
-echo ""
-read -n 1 -s -r -p "Press any key to exit"
-rm -rf rootvps.sh
-exit
-cd
-fi
+#!/bin/bash
+# Ganti password root
+echo "Masukkan password baru untuk root:"
+read -s password
+# Ubah password root
+echo "root:$password" | chpasswd
+# Konfigurasi akses SSH
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+# Restart layanan SSH
+service ssh restart
+echo "Password root telah diubah dan akses SSH telah dikonfigurasi."
